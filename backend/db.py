19cece876a693f3498e6c3cf95aa72e9f7f2a737
Lukas Rönnberg <lukas.e.e.ronnberg@gmail.com>
2025-08-22 17:57:57 +0200

    chore: Dockerize + DATABASE_URL support

--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,10 @@
 # backend/db.py
 import os
-
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
